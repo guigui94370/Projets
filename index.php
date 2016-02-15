375f5ebb3625d96dc8f8e26edfb6e6c39fc2645f
guigui94370 <guillaume.boudy@gmail.com>
2016-02-15 01:19:38 +0100

    Création index.php
    index.php
    Première page, premier commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Document Test</title>
+</head>
+<body>
+	<header><h1>Titre 1</h1></header>
+	<div class="container">Contenu</div>
+	<footer>Mon footer</footer>
+</body>
+</html>
